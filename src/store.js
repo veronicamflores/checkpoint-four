7a71777071a61f5f151670ebf628dc1e5067fee0
Veronica Flores <veronica.marie.flores@gmail.com>
2018-08-29 15:05:06 -0600

    push

--- src/store.js
+++ src/store.js
@@ -1,16 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import router from './router'
+
+let gameApi = axios.create({
+  baseURL: 'https://inspire-server.herokuapp.com/cards',
+  timeout: 3000
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    game:{},
+    player:[],
+    target:[],
+    targets:{},
+    players:{}
 
   },
   mutations: {
-
+    setPlayers(){},
+    setPlayer(){},
+    setTargets(){},
+    setTarget(){},
+    setGame(){}
   },
   actions: {
-
+    getGame({commit, dispatch}, gameId){
+      gameApi.get('cards/' +gameId)
+      .then(res=>{
+        commit('setGame', res.data)
+      })
+    },
+    startGame({commit, dispatch}, gameConfig){
+      gameApi.post('cards',{ playerId: gameConfig.player.id, targetId: gameConfig.target.id } )
+      .then(res=>{
+        commit('setGame', res.data.game)
+      })
+    }
   }
 })
