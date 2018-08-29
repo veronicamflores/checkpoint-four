2bc32f067e7a50030d307dac0cd8c15849f94e40
Veronica Flores <veronica.marie.flores@gmail.com>
2018-08-29 16:51:13 -0600

    push

---
+++ src/components/Player.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="players">
+      <div class="loading" v-if="players.length == 0">loading players please wait</div>
+      <div class="target" v-else v-for="player in players" :key="player.id" @click="setPlayer(player)">
+        {{player.name}}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$store.dispatch("getPlayers");
+  },
+  computed: {
+    Players() {
+      return this.$store.state.players;
+    }
+  },
+  methods: {
+    setPlayer(player) {
+      this.$store.dispatch("setPlayer",player);
+    }
+  }
+};
+</script>
---
+++ src/views/Game.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="game">
+        <form @sumbit="game">
+            <label for=""></label>
+            <input type="text" v-model="playerName"/>
+            <button type="submit">Sumbit</button>
+        </form>
+        <div class="game-config">
+            <div class="selected-player"></div>
+            <div>
+                <button @click="startGame"></button>
+            </div>
+            <div class="selected-target"></div>
+        </div>
+        <div class="select-game-details">
+            <targets/>
+            <players/>
+        </div>
+    </div>
+</template>
+<script>
+import Player from "@/components/Player"
+import Target from "@/components/Target"
+export default {
+    
+}
+</script>
